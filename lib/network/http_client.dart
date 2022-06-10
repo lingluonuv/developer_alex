db891a82111cff4a63ba9b653e7d6edfe508af74
gaolei <gaolei@flyele.net>
2022-06-10 15:20:11 +0800

    fix:fix interceptor bug

--- lib/network/http_client.dart
+++ lib/network/http_client.dart
@@ -27,8 +27,8 @@ class HTTPClient {
     Options? options,
     CancelToken? cancelToken,
     ProgressCallback? onReceiveProgress,
-  }) async {
-    return await _dio.get(path, queryParameters: queryParameters, options: options, cancelToken: cancelToken,);
+  }) {
+    return _dio.get(path, queryParameters: queryParameters, options: options, cancelToken: cancelToken,);
   }
 
   Future post(
@@ -39,8 +39,8 @@ class HTTPClient {
     CancelToken? cancelToken,
     ProgressCallback? onSendProgress,
     ProgressCallback? onReceiveProgress,
-  }) async {
-    return await _dio.post(
+  }) {
+    return _dio.post(
       path,
       queryParameters: queryParameters,
       options: options,
--- lib/network/http_interceptor.dart
+++ lib/network/http_interceptor.dart
@@ -1,18 +1,29 @@
+import 'package:developer_alex/app_config.dart';
 import 'package:dio/dio.dart';
 
 class HttpInterceptor extends Interceptor {
-  // @override
-  // void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-  //   super.onRequest(options, handler);
-  // }
-  //
-  // @override
-  // void onResponse(Response response, ResponseInterceptorHandler handler) {
-  // }
-  //
-  // @override
-  // void onError(DioError err, ErrorInterceptorHandler handler) {
-  //   super.onError(err, handler);
-  // }
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    if (AppConfig.isDebug) {
+      print('Dio on request');
+    }
+    handler.next(options);
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    if (AppConfig.isDebug) {
+      print('Dio on response');
+    }
+    handler.next(response);
+  }
+
+  @override
+  void onError(DioError err, ErrorInterceptorHandler handler) {
+    if (AppConfig.isDebug) {
+      print('Dio on err');
+    }
+    handler.next(err);
+  }
 
 }
